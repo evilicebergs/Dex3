103dbb93d6aff324e06f513c67d80e9d85a25558
evilicebergs <voiced.fair-0a@icloud.com>
2024-09-27 14:48:25 +0300

    add PokemonDetail and SamplePokemon to use sample in further files

---
+++ Dex3/PokemonDetail.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonDetail.swift
+//  Dex3
+//
+//  Created by Artem Golovchenko on 2024-09-27.
+//
+
+import SwiftUI
+
+struct PokemonDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonDetail()
+}
---
+++ Dex3/SamplePokemon.swift
@@ -0,0 +1,8 @@
+//
+//  SamplePokemon.swift
+//  Dex3
+//
+//  Created by Artem Golovchenko on 2024-09-27.
+//
+
+import Foundation
