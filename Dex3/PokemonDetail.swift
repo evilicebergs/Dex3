c4df26a799b85db0f7894dbc806aaddf4c5335d0
evilicebergs <voiced.fair-0a@icloud.com>
2024-09-27 16:06:00 +0300

    extend Pokemon model(add changes from last commit)

--- Dex3/PokemonDetail.swift
+++ Dex3/PokemonDetail.swift
@@ -6,13 +6,68 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct PokemonDetail: View {
+    
+    @EnvironmentObject var pokemon: Pokemon
+    
+    @State var showShiny = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            ZStack {
+                Image(pokemon.background)
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: .black, radius: 6)
+                AsyncImage(url: showShiny ? pokemon.shiny : pokemon.sprite) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.top, 50)
+                        .shadow(color: .black, radius: 6)
+                } placeholder: {
+                    ProgressView()
+                }
+            }
+            HStack {
+                ForEach(pokemon.types!, id: \.self) { type in
+                    Text(type.capitalized)
+                        .font(.title2)
+                        .shadow(color: .white, radius: 2)
+                        .padding([.top, .bottom], 7)
+                        .padding([.leading, .trailing])
+                        .background(Color(type.capitalized))
+                        .clipShape(.rect(cornerRadius: 50))
+                    
+                }
+                Spacer()
+            }
+            .padding()
+            
+        }
+        .navigationTitle(pokemon.name!.capitalized)
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    showShiny.toggle()
+                } label: {
+                    Image(systemName: showShiny ? "wand.and.stars" : "wamd.and.stars.inverse")
+                        .foregroundStyle(showShiny ? .yellow : .clear)
+//                    if showShiny {
+//                        Image(systemName: "wand.and.stars")
+//                            .foregroundStyle(.yellow)
+//                    } else {
+//                        Image(systemName: "wamd.and.stars.inverse")
+//                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
     PokemonDetail()
+        .environmentObject(SamplePokemon.samplePokemon)
 }
--- Dex3/SamplePokemon.swift
+++ Dex3/SamplePokemon.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import CoreData
+
+struct SamplePokemon {
+    //value context should be in the main actor
+    @MainActor static let samplePokemon = {
+            let context = PersistenceController.preview.container.viewContext
+            let fetchRequest: NSFetchRequest<Pokemon> = Pokemon.fetchRequest()
+            fetchRequest.fetchLimit = 1
+            
+            let results = try! context.fetch(fetchRequest)
+            return results.first!
+    }()
+}
